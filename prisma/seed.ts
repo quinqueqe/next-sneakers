8f95e694ed09f412a1313b9588b36d04bf18808f
asecer <vrsxdcrown@gmail.com>
2025-05-15 19:52:22 +0300

    (prisma) finalized the scheme (may need to be modified), started working with seed.ts

---
+++ prisma/seed.ts
@@ -0,0 +1,46 @@
+import prisma from './prisma'
+import { hashSync } from 'bcrypt'
+
+async function up() {
+	await prisma.user.createMany({
+		data: [
+			{
+				fullName: 'User',
+				email: 'user@test.ru',
+				password: hashSync('111111', 10),
+				verified: new Date(),
+				role: 'USER',
+			},
+			{
+				fullName: 'Admin',
+				email: 'admin@test.ru',
+				password: hashSync('111111', 10),
+				verified: new Date(),
+				role: 'ADMIN',
+			},
+		],
+	})
+}
+
+async function down() {
+	await prisma.user.deleteMany() // вместо raw-запроса
+}
+
+async function main() {
+	try {
+		await down() // очищаем базу данных
+		await up() // заполняем базу данных
+	} catch (e) {
+		console.log(e)
+	}
+}
+
+main() // Promise
+	.then(async () => {
+		await prisma.$disconnect
+	})
+	.catch(async e => {
+		console.error(e)
+		await prisma.$disconnect()
+		process.exit(1)
+	})
