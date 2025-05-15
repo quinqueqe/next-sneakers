aee6bec8a9ad359af406831e3846b23ed446e7a4
asecer <vrsxdcrown@gmail.com>
2025-05-15 20:09:02 +0300

    (api) made a folder with client api and customized it

---
+++ services/products.ts
@@ -0,0 +1,12 @@
+import { Product } from '@prisma/client'
+import { axiosInstance } from './instance'
+import { ApiRoutes } from './constants'
+
+export const search = async (q: string) => {
+	// : Promise<Product[]>
+	return (
+		await axiosInstance.get<Product[]>(ApiRoutes.SEARCH_PRODUCTS, {
+			params: { q }, // получит все то что идет после '?'
+		})
+	).data
+}
