b0e3be5d18e059d0edec2779879710c888e0c1a6
asecer <vrsxdcrown@gmail.com>
2025-06-09 13:11:22 +0300

    (zustand, react-use, header) added new states to useSearch and connected react-use, configuring search-product closure

--- shared/components/shared/search-product.tsx
+++ shared/components/shared/search-product.tsx
@@ -2,19 +2,31 @@
 
 import React from 'react'
 import { Search } from 'lucide-react'
+import { useSearch } from '@/shared/store/search'
+import { useClickAway } from 'react-use'
 
 export const SearchProduct = () => {
-	const focus = false
+	const { value, focus, setValue, setFocus } = useSearch()
+	const ref = React.useRef(null)
+	useClickAway(ref, () => {
+		setFocus(false)
+	})
 	return (
 		<div>
 			{focus && (
 				<div className='fixed top-0 left-0 bottom-0 right-0 bg-black/50 z-30' />
 			)}
-			<div className='flex relative z-31 items-center pl-5 py-4 rounded-2xl bg-[#f9f9f9] w-[650px] max-w-[650px] '>
+			<div
+				ref={ref}
+				className='flex relative z-31 items-center pl-5 py-4 rounded-2xl bg-[#f9f9f9] w-[650px] max-w-[650px] '
+			>
 				<div className='border-r-2 pr-3'>
 					<Search size={18} className='text-[#c0c0c0] ' />
 				</div>
 				<input
+					onClick={() => setFocus(true)}
+					value={value}
+					onChange={e => setValue(e.target.value)}
 					type='text'
 					placeholder='Поиск...'
 					className='placeholder:text-[#c0c0c0] pl-3 w-[764px] max-w-[764px]'
--- shared/store/search.ts
+++ shared/store/search.ts
@@ -2,10 +2,14 @@ import { create } from 'zustand'
 
 type SearchType = {
 	value: string
+	focus: boolean
 	setValue: (value: string) => void
+	setFocus: (value: boolean) => void
 }
 
 export const useSearch = create<SearchType>(set => ({
 	value: '',
+	focus: false,
 	setValue: value => set({ value }),
+	setFocus: value => set({ focus: value }),
 }))
