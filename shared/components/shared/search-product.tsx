48227d95e5d8e33e72a89a753c6ca5ba54fb7609
asecer <vrsxdcrown@gmail.com>
2025-06-09 15:05:52 +0300

    (header) finalized the work of search in the header, made the output of products

--- shared/components/shared/search-product.tsx
+++ shared/components/shared/search-product.tsx
@@ -5,6 +5,8 @@ import { Search } from 'lucide-react'
 import { useSearch } from '@/shared/store/search'
 import { useClickAway, useDebounce } from 'react-use'
 import { Api } from '../services/api-client'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export const SearchProduct = () => {
 	const { value, focus, products, setValue, setFocus, setProducts } =
@@ -28,13 +30,13 @@ export const SearchProduct = () => {
 		[value]
 	)
 	return (
-		<div>
+		<div className='relative'>
 			{focus && (
 				<div className='fixed top-0 left-0 bottom-0 right-0 bg-black/50 z-30' />
 			)}
 			<div
 				ref={ref}
-				className='flex relative z-31 items-center pl-5 py-4 rounded-2xl bg-[#f9f9f9] w-[650px] max-w-[650px]'
+				className='flex relative z-31 items-center pl-5 py-4 rounded-2xl bg-[#f9f9f9] w-[650px]'
 			>
 				<div className='border-r-2 pr-3'>
 					<Search size={18} className='text-[#c0c0c0] ' />
@@ -48,6 +50,25 @@ export const SearchProduct = () => {
 					className='placeholder:text-[#c0c0c0] pl-3 w-[764px] max-w-[764px]'
 				/>
 			</div>
+			<ul className='w-[650px] absolute top-16 left-0 bg-white z-99 rounded-2xl py-2'>
+				{focus &&
+					products.map(product => (
+						<Link href={`/product/${product.id}`} key={product.id}>
+							<li className='flex items-center justify-between rounded-2xl px-3 py-2'>
+								<div className='flex gap-3 items-center'>
+									<Image
+										src={product.imageUrl}
+										alt='img'
+										width={40}
+										height={40}
+									/>
+									<p className='text-[16px] '>{product.name}</p>
+								</div>
+								<p className='text-[14px] font-bold'>{product.price}₽</p>
+							</li>
+						</Link>
+					))}
+			</ul>
 		</div>
 	)
 }
